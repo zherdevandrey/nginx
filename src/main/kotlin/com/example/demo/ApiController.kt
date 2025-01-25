637b23ff48daac25937300ff458587d6b39e2b12
zherdev <andrey.zherdev@db.com>
2025-01-25 22:01:14 +0300

    routing by location

--- src/main/kotlin/com/example/demo/ApiController.kt
+++ src/main/kotlin/com/example/demo/ApiController.kt
@@ -7,9 +7,16 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ApiController {
 
-    @GetMapping("/api")
-    fun getApi(request: HttpServletRequest): MutableMap<String, String> {
-        return mutableMap(request)
+    @GetMapping("/api1")
+    fun getApi1(): String {
+        println("Get api1 request")
+        return "nginx api1 response"
+    }
+
+    @GetMapping("/api2")
+    fun getApi2(): String {
+        println("Get api2 request")
+        return "nginx api2 response"
     }
 
     @GetMapping
